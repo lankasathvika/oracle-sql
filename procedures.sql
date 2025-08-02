e79923936454e261fc36924d270fddc9effe06c1
lankasathvika <90507761+lankasathvika@users.noreply.github.com>
2025-08-02 16:45:22 +0530

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -14,3 +14,10 @@ CREATE OR REPLACE PROCEDURE get_square(num IN NUMBER, result OUT NUMBER)
 
 ----IN OUT Parameter Procedure
 CREATE OR REPLACE PROCEDURE double_number(num IN OUT NUMBER)
+
+----stored procedure
+CREATE OR REPLACE PROCEDURE send_birthday_wish(name IN VARCHAR2) IS
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('🎉 Happy Birthday, ' || name || '! 🎂');
+END;
+
