bcfdff2c774e878926521c330fc3890a91a2b0bb
lankasathvika <90507761+lankasathvika@users.noreply.github.com>
2025-08-02 17:20:52 +0530

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -16,8 +16,35 @@ CREATE OR REPLACE PROCEDURE get_square(num IN NUMBER, result OUT NUMBER)
 CREATE OR REPLACE PROCEDURE double_number(num IN OUT NUMBER)
 
 ----stored procedure
-CREATE OR REPLACE PROCEDURE send_birthday_wish(name IN VARCHAR2) IS
+CREATE TABLE departments (
+  dept_id NUMBER PRIMARY KEY,              
+  dept_name VARCHAR2(50)                    
+);
+
+CREATE TABLE students (
+  id NUMBER PRIMARY KEY,            
+  name VARCHAR2(50),                      
+  dept_id NUMBER,                         
+  FOREIGN KEY (dept_id) REFERENCES departments(dept_id)
+);
+INSERT INTO departments VALUES (1, 'CSE');
+INSERT INTO students VALUES (101, 'Sathvika', 1);
+
+CREATE OR REPLACE PROCEDURE get_dept_name(p_id IN NUMBER) IS
+  v_dept_name departments.dept_name%TYPE;
 BEGIN
-  DBMS_OUTPUT.PUT_LINE('🎉 Happy Birthday, ' || name || '! 🎂');
+  SELECT d.dept_name
+  INTO v_dept_name
+  FROM students s
+  JOIN departments d ON s.dept_id = d.dept_id
+  WHERE s.id = p_id;
+
+  DBMS_OUTPUT.PUT_LINE('Department: ' || v_dept_name);
 END;
+
+  
+
+
+
+
 
